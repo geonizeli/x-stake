fe072595ed2e7eb061b0148967e6f686fb6272a1
João Geonizeli <joao.geonizeli@gmail.com>
2021-08-09 23:13:05 -0300

    fix primary button color

--- app/helpers/tailwind_helper.rb
+++ app/helpers/tailwind_helper.rb
@@ -11,7 +11,7 @@ module TailwindHelper
     end
 
     def primary_button_classes
-      "#{base_button_classes} bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
+      "#{base_button_classes} bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
     end
   end
 end
