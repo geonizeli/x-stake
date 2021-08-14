51a493b327b85673c680ec3a13e77369972bce3b
João Geonizeli <joao.geonizeli@gmail.com>
2021-08-14 11:45:25 -0300

    remove unused GraphQl input

--- app/graphql/inputs/user_attributes_input.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-module Inputs
-  class UserAttributesInput < Types::BaseInputObject
-    graphql_name "UserAttributesInput"
-
-    argument :first_name, String, required: true
-    argument :last_name, String, required: true
-  end
-end
