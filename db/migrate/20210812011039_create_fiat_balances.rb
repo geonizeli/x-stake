de5e581f2130b3dd3b71c6bb79831d4c5c446b21
João Geonizeli <joao.geonizeli@gmail.com>
2021-08-14 13:51:00 -0300

    remove monetize helper from CreateFiatBalances migration

--- db/migrate/20210812011039_create_fiat_balances.rb
+++ db/migrate/20210812011039_create_fiat_balances.rb
@@ -3,7 +3,9 @@ class CreateFiatBalances < ActiveRecord::Migration[6.1]
   def change
     create_table(:fiat_balances) do |t|
       t.references(:user, null: false, foreign_key: true)
-      t.monetize(:amount)
+
+      t.integer(:amount_cents, null: false, default: 0)
+      t.string(:amount_currency, null: false, default: "BRL")
 
       t.timestamps
     end
